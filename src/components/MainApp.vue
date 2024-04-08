76ff55d52f648e4fd7500ac8aa9875d18cc123a4
chrialge <chrialge99@gmail.com>
2024-04-08 15:16:41 +0200

    add section our consultation

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -12,13 +12,54 @@ export default {
 <template>
     <section id="jumbotron">
         <div class="left">
-            <h1>Lorem, ipsum.</h1>
+            <h1>Lorem, ipsum</h1>
         </div>
         <div class="right">
             <h1>Lorem, ipsum.</h1>
         </div>
     </section>
     <!-- /#jumbotron -->
+
+    <section id="our-consultation">
+        <div class="container">
+            <div class="title">
+                <h1>OUR CONSULTANTS CAN HELP YOU</h1>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <div class="left">Lorem.</div>
+                    <div class="right">
+                        <h1>Lorem, ipsum.</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sit?</p>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="left">Lorem.</div>
+                    <div class="right">
+                        <h1>Lorem, ipsum.</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sit?</p>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="left">Lorem.</div>
+                    <div class="right">
+                        <h1>Lorem, ipsum.</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sit?</p>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="left">Lorem.</div>
+                    <div class="right">
+                        <h1>Lorem, ipsum.</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sit?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- /#our-consultation -->
 
 </template>
 
