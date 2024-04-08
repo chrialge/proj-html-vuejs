516d45d1a4ae18d3da7977309cce2425e90415e7
chrialge <chrialge99@gmail.com>
2024-04-08 15:59:02 +0200

    add section about-us and project

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -61,6 +61,57 @@ export default {
     </section>
     <!-- /#our-consultation -->
 
+    <section id="about-us">
+        <div class="container">
+            <div class="left">Lorem.</div>
+            <div class="right">
+                <div class="section">
+                    <hr>
+                    <span>ABOUT US</span>
+                </div>
+                <h1>WE MAKE YOUR STRATEGY WORK</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam distinctio voluptas veniam, qui rem
+                    corrupti quo eos quisquam reiciendis dolor.
+                </p>
+            </div>
+            <div class="banner">
+                <div class="left_banner">
+                    <span>Lorem ipsum dolor sit.</span>
+                </div>
+                <div class="right_banner">
+                    <p>Lorem ipsum dolor sit.</p>
+
+                </div>
+                <button>get started</button>
+            </div>
+        </div>
+
+    </section>
+    <!-- /#about-us -->
+
+    <section id="project">
+        <div class="container">
+            <div class="section">
+                <hr>
+                <span>PROJECT</span>
+            </div>
+            <div class="header-project">
+                <h1>OUR EXPERT TRUSTED CONSULTANTS HELP CLIENTS</h1>
+                <div class="btn-prev-next">
+                    <button>prev</button>
+                    <button>next</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">Lorem, ipsum.</div>
+                <div class="col-3">Dolores, quisquam?</div>
+                <div class="col-3">Fugit, nemo?</div>
+            </div>
+
+        </div>
+    </section>
+    <!-- /#project -->
 </template>
 
 <style></style>
