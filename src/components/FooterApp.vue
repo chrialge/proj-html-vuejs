f9a91b3d1d7d8318d9df3a1f01464c18ee0f7655
chrialge <chrialge99@gmail.com>
2024-04-08 17:00:14 +0200

    add footer

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -10,10 +10,44 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>Sono il footer</h1>
-        <p>{{ msg }}</p>
-    </div>
+    <footer id="site_footer">
+        <div class="container">
+            <div class="footer_top">
+                <div class="logo">Lorem.</div>
+                <div class="navbar">
+                    <ul>
+                        <li><a href="#">Lorem.</a></li>
+                        <li><a href="#">Perspiciatis?</a></li>
+                        <li><a href="#">Eius.</a></li>
+                        <li><a href="#">Magnam!</a></li>
+                        <li><a href="#">Non.</a></li>
+                        <li><a href="#">Nam.</a></li>
+                    </ul>
+                </div>
+                <div class="social">
+                    <ul>
+                        <li><a href="">Lorem.</a></li>
+                        <li><a href="">Suscipit.</a></li>
+                        <li><a href="">Nisi!</a></li>
+                        <li><a href="">Similique.</a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class="footer-bottom">
+                <div class="policy">
+                    <ul>
+                        <li><a href="#">Lorem.</a></li>
+                        <li><a href="#">Quia?</a></li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    Copyright &copy; 2020 Phlox Consulting.All Rights Reserved.
+                </div>
+            </div>
+        </div>
+
+    </footer>
 </template>
 
 <style></style>
