3cd6f632c9b1c91a099750f0f7b26e1319c4d462
chrialge <chrialge99@gmail.com>
2024-10-28 19:48:58 +0100

    add responsive for header, footer, jumbotron, testimonial, about us, our consultation

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -9,6 +9,16 @@ export default {
   },
   props: {
     navbar: Array,
+  },
+  methods: {
+    openSiderbar() {
+      document.getElementById("overlay").style.visibility = "visible"
+      document.getElementById("overlay").style.opacity = 1;
+    },
+    closeSiderbar() {
+      document.getElementById("overlay").style.visibility = ""
+      document.getElementById("overlay").style.opacity = '';
+    }
   }
 }
 </script>
@@ -24,12 +34,22 @@ export default {
       <!-- /logo -->
 
       <!-- navbar -->
-      <div class="navbar">
+      <div class="navbar" >
         <ul>
           <li v-for="section in navbar"><a :href="section.href">{{ section.name }}</a></li>
         </ul>
       </div>
       <!-- /navbar -->
+
+      <div class="sidebar-mobile">
+        <i class="fa-solid fa-bars" @click="openSiderbar()"></i>
+        <div class="overlay" id="overlay">
+          <i class="fa fa-close" aria-hidden="true" @click="closeSiderbar()"></i>
+          <ul>
+            <li v-for="section in navbar"><a :href="section.href">{{ section.name }}</a></li>
+          </ul>
+        </div>
+      </div>
 
     </nav>
   </header>
--- src/components/components-section/AboutUs.vue
+++ src/components/components-section/AboutUs.vue
@@ -11,23 +11,28 @@ export default {
 <template>
     <section id="about-us">
         <div class="container_fluid d-flex">
-            <div class="left col-6">
-                <img src="/images/business-people-working-together-on-project-and-5FHSKBL.jpg"
-                    alt="people working together on project">
-            </div>
-            <div class="right col-5">
-                <div class="section">
-                    <div class="separeted_small"></div>
-                    <span class="space-letter">ABOUT US</span>
+            <div class="d-flex top-about-us">
+                <div class="left col-6">
+                    <img src="/images/business-people-working-together-on-project-and-5FHSKBL.jpg"
+                        alt="people working together on project">
                 </div>
-                <h1>WE MAKE YOUR STRATEGY WORK</h1>
-                <p class="paragraph">
-                    When, while lovely valley teems with vapour around meand eridian sun strikes the upper
-                    impernetraable foliage of my trees, and but a thousand unknow plant are noticed when i hear the buzz
-                    of the little world among the stalks and grow familiar indescribable forms of the insects and files,
-                    the i feel the presence of the almighty
-                </p>
+                <div class="right col-5">
+                    <div class="section">
+                        <div class="separeted_small"></div>
+                        <span class="space-letter">ABOUT US</span>
+                    </div>
+                    <h1>WE MAKE YOUR STRATEGY WORK</h1>
+                    <p class="paragraph">
+                        When, while lovely valley teems with vapour around meand eridian sun strikes the upper
+                        impernetraable foliage of my trees, and but a thousand unknow plant are noticed when i hear the
+                        buzz
+                        of the little world among the stalks and grow familiar indescribable forms of the insects and
+                        files,
+                        the i feel the presence of the almighty
+                    </p>
+                </div>
             </div>
+
             <div class="banner d-flex col-11 posi-rela">
                 <div class="left_banner">
                     <h1>WE HELP TO DELIVER THE BUSINESS CASE</h1>
