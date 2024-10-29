f46506bfae3325569b929a628518d1e75b201e4a
chrialge <chrialge99@gmail.com>
2024-10-29 12:38:18 +0100

    add new logic for slider and set for responsive

--- src/components/components-section/Project.vue
+++ src/components/components-section/Project.vue
@@ -4,6 +4,7 @@ export default {
     data() {
         return {
             activeImage: [0, 1, 2],
+            activeImageMobile: 0,
             images: [
                 { id: 0, img: 'DRY-1-790x592.jpg' },
                 { id: 1, img: '221bf0b7-8134-43bb-936a-5acbe42db64a-790x592.jpg' },
@@ -19,40 +20,225 @@ export default {
          * funzione che manda indietrole immagini dello slider del testimonial
          */
         prev() {
-            const length = this.images.length;
-            let incremental = this.activeImage.map((number) => {
-                number--;
-                return number;
-            })
-            if (incremental.includes(-1)) {
-                incremental = [2, 3, 4]
-            }
+            const widthDevice = window.innerWidth;
 
-            // console.log(incremental);
-            this.activeImage = incremental;
-            // console.log(this.activeImage);
+            const containerCrad = document.querySelector('#project>.container>.row');
+            const boxImageEl = document.querySelectorAll(".box-image");
+
+            if (widthDevice <= 900) {
+
+
+                if (this.activeImageMobile === 0) {
+                    this.activeImage = this.images.length - 1
+
+                    this.images.forEach((img, index) => {
+                        if (index === this.activeImageMobile) {
+
+                            boxImageEl[0].setAttribute('src', `./images/${img.img}`)
+                        }
+                    })
+
+
+                } else {
+                    this.activeImageMobile--
+
+
+                    this.images.forEach((img, index) => {
+                        if (index === this.activeImageMobile) {
+
+                            boxImageEl[0].setAttribute('src', `./images/${img.img}`)
+                        }
+                    })
+
+
+
+                }
+            } else {
+                console.log(document.querySelectorAll(".box-image"));
+
+
+
+
+
+                const numberImage = this.activeImage.map((number) => {
+                    if (number === (this.images.length - 1)) {
+                        return 4
+                    } else {
+                        return number - 1
+                    }
+
+                })
+                this.activeImage = numberImage
+
+                console.log(this.activeImage, numberImage);
+
+                let count = 0
+
+                this.activeImage.forEach((imagenew) => {
+
+
+                    this.images.forEach((img, index) => {
+                        if (index === imagenew) {
+                            boxImageEl[count].setAttribute('src', `./images/${img.img}`)
+                            count++
+
+                        }
+                    })
+                })
+
+
+
+            }
         },
 
         /**
         * funzione che manda avanti le immagini dello slider del testimonial
         */
         next() {
-            const length = this.images.length;
-            let incremental = this.activeImage.map((number) => {
-                number++;
+            const widthDevice = window.innerWidth;
 
-                return number;
-            })
-            if (incremental.includes(length)) {
-                incremental = [0, 1, 2];
+            const containerCrad = document.querySelector('#project>.container>.row');
+            const boxImageEl = document.querySelectorAll(".box-image");
+
+            if (widthDevice <= 900) {
+                console.log(this.activeImageMobile)
+
+
+                if (this.activeImageMobile === (this.images.length - 1)) {
+                    this.activeImageMobile = 0
+
+                    this.images.forEach((img, index) => {
+                        if (index === this.activeImageMobile) {
+
+                            boxImageEl[0].setAttribute('src', `./images/${img.img}`)
+                        }
+                    })
+
+
+                } else {
+                    this.activeImageMobile++
+
+
+                    this.images.forEach((img, index) => {
+                        if (index === this.activeImageMobile) {
+
+                            boxImageEl[0].setAttribute('src', `./images/${img.img}`)
+                        }
+                    })
+
+
+
+                }
+            } else {
+                console.log(document.querySelectorAll(".box-image"));
+
+
+
+
+
+                const numberImage = this.activeImage.map((number) => {
+                    if (number === 4) {
+                        return 0
+                    } else {
+                        return number + 1
+                    }
+
+                })
+                this.activeImage = numberImage
+
+                console.log(this.activeImage, numberImage);
+
+                let count = 0
+
+                this.activeImage.forEach((imagenew) => {
+
+
+                    this.images.forEach((img, index) => {
+                        if (index === imagenew) {
+                            boxImageEl[count].setAttribute('src', `./images/${img.img}`)
+                            count++
+
+                        }
+                    })
+                })
+
+
+
             }
-            // console.log(incremental);
-            this.activeImage = incremental;
-            // console.log(this.activeImage);
+
         },
 
     },
     mounted() {
+        const containerCrad = document.querySelector('#project>.container>.row');
+
+
+
+        const widthDevice = window.innerWidth;
+        // const cardEl = document.querySelectorAll('.card-project');
+
+        if (widthDevice < 900) {
+
+
+            this.images.forEach((img, index) => {
+                if (index === this.activeImageMobile) {
+
+                    const markup = `
+                        <div class="col-4 posi-rela card-project" >
+                    <img src="./images/${img.img}" alt="" class="box-image">
+                    <div class="overlay d-flex">
+                        <div class="info-product d-flex">
+                            <div class="info">
+                                <h3>Purinky Products</h3>
+                                <span>uncatogorized</span>
+                            </div>
+                            <div class="plus">
+                                <i class="fa-solid fa-plus"></i>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                    `
+                    containerCrad.insertAdjacentHTML('beforeend', markup)
+                }
+            })
+
+        } else {
+
+            this.activeImage.forEach((imageActive) => {
+                // console.log(imageActive)
+
+                this.images.forEach((img, index) => {
+                    if (imageActive === index) {
+
+                        const markup = `
+                        <div class="col-4 posi-rela card-project" >
+                    <img src="./images/${img.img}" alt="" class="box-image">
+                    <div class="overlay d-flex">
+                        <div class="info-product d-flex">
+                            <div class="info">
+                                <h3>Purinky Products</h3>
+                                <span>uncatogorized</span>
+                            </div>
+                            <div class="plus">
+                                <i class="fa-solid fa-plus"></i>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                    `
+                        containerCrad.insertAdjacentHTML('beforeend', markup)
+                    }
+                })
+            })
+
+
+
+        }
+
+
 
     }
 }
@@ -77,22 +263,7 @@ export default {
                 </div>
             </div>
             <div class="row d-flex gap-1">
-                <div class="col-4 posi-rela" v-for="img in images"
-                    :class="{ 'display-none': !activeImage.includes(img.id) }">
-                    <img :src="'/images/' + img.img" alt="">
-                    <div class="overlay d-flex">
-                        <div class="info-product d-flex">
-                            <div class="info">
-                                <h3>Purinky Products</h3>
-                                <span>uncatogorized</span>
-                            </div>
-                            <div class="plus">
-                                <i class="fa-solid fa-plus"></i>
-                            </div>
-                        </div>
 
-                    </div>
-                </div>
             </div>
 
         </div>
